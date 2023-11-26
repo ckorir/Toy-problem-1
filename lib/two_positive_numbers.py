3f375c0b6747890e3fc2f4f8dcb07bb2759dc966
ckorir <chrisruto.k@gmail.com>
2023-11-26 10:52:18 +0300

    Finished two positive numbers problem

---
+++ lib/two_positive_numbers.py
@@ -0,0 +1,15 @@
+def two_positive(a, b, c):
+
+    # Count the number of positive integers
+    count_positive = sum(x > 0 for x in (a, b, c))
+
+    # Check if exactly two of the three integers are positive
+    return count_positive == 2
+
+# Get user Input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+result = two_positive(a, b, c)
+print(f"The result is: {result}")
